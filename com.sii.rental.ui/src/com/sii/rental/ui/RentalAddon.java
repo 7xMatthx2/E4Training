54c49ed6d42619588361fb1bff186452c809da84
VAELIA <VAELIA@10.131.100.96>
2017-03-16 10:22:14 +0100

    Lecture des extensions du point d'extension org.eclipse.e4.workbench.model

--- com.sii.rental.ui/src/com/sii/rental/ui/RentalAddon.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/RentalAddon.java
@@ -1,13 +1,19 @@
 package com.sii.rental.ui;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
+import com.opcoach.training.rental.Customer;
 import com.opcoach.training.rental.RentalAgency;
 import com.sii.rental.core.RentalCoreActivator;
 
@@ -31,5 +37,31 @@ public class RentalAddon implements RentalUIConstants{
 		reg.put(IMG_AGENCY, ImageDescriptor.createFromURL(b.getEntry(IMG_AGENCY)));
 		
 		return reg;
+	}
+	
+	@Inject @Optional
+	void reactOnRentalEvent(@UIEventTopic("rental/copy") Customer customer)
+	{
+		System.out.println("Copie du client " + customer.getDisplayName());
+	}
+	
+	@Inject
+	public void getExtensions(IExtensionRegistry reg)
+	{
+		for(IConfigurationElement elt : reg.getConfigurationElementsFor("org.eclipse.e4.workbench.model"))
+		{
+			
+			System.out.print("Point d'extension trouvé. ");
+			if(elt.getName().equals("fragment"))
+			{
+				String attValue = elt.getAttribute("uri");
+				System.out.println("Fragment : " + attValue);
+			}
+			else
+			{
+				String attValue = elt.getAttribute("class");
+				System.out.println("Processeur : " + attValue);
+			}
+		}
 	}
 }
--- com.sii.rental.ui/src/com/sii/rental/ui/handlers/CopyHandler.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/handlers/CopyHandler.java
@@ -4,6 +4,7 @@ import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.RTFTransfer;
@@ -22,7 +23,7 @@ public class CopyHandler {
 	}
 
 	@Execute
-	public void execute(@Named(IServiceConstants.ACTIVE_SELECTION) Customer customer)
+	public void execute(@Named(IServiceConstants.ACTIVE_SELECTION) Customer customer, IEventBroker broker)
 	{
 		Clipboard clipboard = new Clipboard(Display.getCurrent());
 		String textData = customer.getDisplayName();
@@ -33,5 +34,7 @@ public class CopyHandler {
 		Object[] data = new Object[]{textData, rtfData};
 		clipboard.setContents(data, transfers);
 		clipboard.dispose();
+		
+		broker.send("rental/copy", customer);
 	}
 }
